7b309f965a6aa63ef17da841ca1e3216bcf36d42
Gaya <gaya.kessler@gmail.com>
2014-07-26 18:44:26 +0200

    First phantimjs browsertests

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     browserify = require('browserify'),
-    source = require('vinyl-source-stream');
+    source = require('vinyl-source-stream'),
+    mochaPhantomJS = require('gulp-mocha-phantomjs');
 
 var config = {
     src: 'src',
@@ -22,7 +23,26 @@ gulp.task('browserify', function() {
         .pipe(gulp.dest(config.dist));
 });
 
-gulp.task('test', function() {
+gulp.task('browserify-tests', function() {
     'use strict';
-    gulp.watch(config.src + "/**/*.js", ["browserify"]);
+    return browserify('./' + config.test + '/tests.js')
+        .bundle()
+        .on('error', function (err) {
+            console.log(err.toString());
+            this.emit('end');
+        })
+        .pipe(source(pkg.name + '-tests.js'))
+        .pipe(gulp.dest(config.dist));
 });
+
+gulp.task('test', function () {
+    'use strict';
+    return gulp
+        .src('test/browser/index.html')
+        .pipe(mochaPhantomJS());
+});
+
+//gulp.task('test', function() {
+//    'use strict';
+//    gulp.watch(config.src + "/**/*.js", ["browserify"]);
+//});
---
+++ test/tests.js
@@ -0,0 +1,9 @@
+var assert = require("assert");
+
+describe('SliderInput', function() {
+    describe('#registerListener()', function () {
+        it('should be able to register a listener', function () {
+            assert.equal(true, true);
+        })
+    });
+});
