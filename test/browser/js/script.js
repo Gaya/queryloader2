b610074a544d9a5520fc64e96c84819c678fbb63
Gaya <gaya.kessler@gmail.com>
2014-08-21 22:18:42 +0200

    bar color testing

--- test/browser/js/script.js
+++ test/browser/js/script.js
@@ -1,6 +1,6 @@
 var ql = new QueryLoader2(document.querySelector("body"), {
     barColor: "#00ff00",
-    backgroundColor: "#ff0000",
+    backgroundColor: "#000000",
     percentage: true,
     barHeight: 1,
     minimumTime: 200,
