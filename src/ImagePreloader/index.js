e9b7ec2cd3026aa2c7874a06b20911c27c14e28b
Gaya Kessler <gaya.kessler@gmail.com>
2014-08-21 16:54:25 +0200

    Make deepSearch for images

--- src/ImagePreloader/index.js
+++ src/ImagePreloader/index.js
@@ -1,12 +1,28 @@
 var Image = require('./Image.js');
 
 function ImagePreloader() {
-
+    "use strict";
+    this.deepSearch = true;
 }
 
 ImagePreloader.prototype.getImages = function (element) {
     "use strict";
+    this.images = [];
+    this.findImageInElement(element);
 
+    if (this.deepSearch === true) {
+        var elements = element.querySelectorAll("*");
+        for (var i = 0; i < elements.length; i++) {
+            if (elements[i].tagName !== "SCRIPT") {
+                this.findImageInElement(elements[i]);
+            }
+        }
+    }
+};
+
+ImagePreloader.prototype.findImageInElement = function (element) {
+    "use strict";
+    this.images = [];
 };
 
 module.exports = ImagePreloader;
