8586d7c4155edaa26fe9230c99fe6d4e726e703a
Gaya Kessler <gaya.kessler@gmail.com>
2014-08-22 10:02:28 +0200

    Adjsuted tests

--- test/browser/js/script.js
+++ test/browser/js/script.js
@@ -1,8 +1,11 @@
-var ql = new QueryLoader2(document.querySelector("body"), {
-    barColor: "#efefef",
-    backgroundColor: "#111",
-    percentage: true,
-    barHeight: 1,
-    minimumTime: 200,
-    fadeOutTime: 1000
+window.addEventListener('DOMContentLoaded', function() {
+    "use strict";
+    var ql = new QueryLoader2(document.querySelector("body"), {
+        barColor: "#efefef",
+        backgroundColor: "#111",
+        percentage: true,
+        barHeight: 1,
+        minimumTime: 200,
+        fadeOutTime: 1000
+    });
 });
