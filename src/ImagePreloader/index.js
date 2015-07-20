5573d8fa081e199440638de61d3bcb0b044f3220
Gaya <gaya.kessler@gmail.com>
2015-07-20 08:13:16 +0200

    formatting code

--- src/ImagePreloader/index.js
+++ src/ImagePreloader/index.js
@@ -90,7 +90,9 @@ ImagePreloader.prototype.determineUrlAndType = function (element) {
     var type = "normal";
     var style = element.currentStyle || window.getComputedStyle(element, null);
 
-    if ((typeof style.backgroundImage != "undefined" && style.backgroundImage !== "" && style.backgroundImage !== "none") || (typeof element.style.backgroundImage != "undefined" && element.style.backgroundImage !== "" && element.style.backgroundImage !== "none")) {
+    if ((typeof style.backgroundImage !== "undefined" && style.backgroundImage !== "" && style.backgroundImage !== "none")
+      || (typeof element.style.backgroundImage !== "undefined" && element.style.backgroundImage !== "" && element.style.backgroundImage !== "none")
+    ) {
         //if object has background image
         url = (style.backgroundImage || element.style.backgroundImage);
         type = "background";
