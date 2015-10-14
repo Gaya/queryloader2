4335fd7f6b9faa4bd6233588fef0a620f0a811f8
Gaya Kessler <gaya.kessler@bluemango.nl>
2015-10-14 16:40:42 +0200

    move functions to loadingbar object

--- src/Overlay/LoadingBar.js
+++ src/Overlay/LoadingBar.js
@@ -1,52 +1,46 @@
 'use strict';
-var LoadingBar = {};
+var LoadingBar = {
+  /**
+   * Creates the element for the loading bar
+   */
+  create: function() {
+    this.element = document.createElement('div');
+    this.element.setAttribute('class', this.className);
+    this.setStyling();
+    this.updateProgress(0, 0);
+  },
 
-/**
- * Creates the element for the loadingbar
- */
-LoadingBar.prototype.create = function () {
-  'use strict';
-  this.element = document.createElement('div');
-  this.element.setAttribute('class', this.className);
-  this.setStyling();
-  this.updateProgress(0, 0);
-};
+  setStyling: function() {
+    this.element.style.height = this.barHeight + 'px';
+    this.element.style.marginTop = '-' + (this.barHeight / 2) + 'px';
+    this.element.style.backgroundColor = this.barColor;
+    this.element.style.position = 'absolute';
+    this.element.style.top = '50%';
 
-LoadingBar.prototype.setStyling = function () {
-  'use strict';
+    this.setTransitionTime(100);
+  },
 
-  this.element.style.height = this.barHeight + 'px';
-  this.element.style.marginTop = '-' + (this.barHeight / 2) + 'px';
-  this.element.style.backgroundColor = this.barColor;
-  this.element.style.position = 'absolute';
-  this.element.style.top = '50%';
+  updateProgress: function(percentage, time) {
+    if (parseInt(percentage) < 0) {
+      percentage = 0;
+    } else if (parseInt(percentage) > 100) {
+      percentage = 100;
+    }
 
-  this.setTransitionTime(100);
-};
+    if (time !== 0) {
+      this.setTransitionTime(time);
+    }
 
-LoadingBar.prototype.updateProgress = function (percentage, time) {
-  'use strict';
+    this.element.style.width = parseInt(percentage) + '%';
+  },
 
-  if (parseInt(percentage) < 0) {
-    percentage = 0;
-  } else if (parseInt(percentage) > 100) {
-    percentage = 100;
-  }
-
-  if (time !== 0) {
-    this.setTransitionTime(time);
-  }
-
-  this.element.style.width = parseInt(percentage) + '%';
-};
-
-LoadingBar.prototype.setTransitionTime = function (ms) {
-  'use strict';
-  this.element.style.WebkitTransition = 'width ' + ms + 'ms';
-  this.element.style.MozTransition = 'width ' + ms + 'ms';
-  this.element.style.OTransition = 'width ' + ms + 'ms';
-  this.element.style.MsTransition = 'width ' + ms + 'ms';
-  this.element.style.Transition = 'width ' + ms + 'ms';
+  setTransitionTime: function(ms) {
+    this.element.style.WebkitTransition = 'width ' + ms + 'ms';
+    this.element.style.MozTransition = 'width ' + ms + 'ms';
+    this.element.style.OTransition = 'width ' + ms + 'ms';
+    this.element.style.MsTransition = 'width ' + ms + 'ms';
+    this.element.style.Transition = 'width ' + ms + 'ms';
+  },
 };
 
 module.exports = function() {
