3cf23a4d869c4a86b8e66c97fbd6a0249e4d4ba1
Gaya Kessler <gaya.kessler@bluemango.nl>
2015-10-14 16:37:36 +0200

    fix indents and quotes

--- src/Overlay/LoadingBar.js
+++ src/Overlay/LoadingBar.js
@@ -1,54 +1,52 @@
 'use strict';
-var LoadingBar = {
-
-};
+var LoadingBar = {};
 
 /**
  * Creates the element for the loadingbar
  */
 LoadingBar.prototype.create = function () {
-    'use strict';
-    this.element = document.createElement("div");
-    this.element.setAttribute("class", this.className);
-    this.setStyling();
-    this.updateProgress(0, 0);
+  'use strict';
+  this.element = document.createElement('div');
+  this.element.setAttribute('class', this.className);
+  this.setStyling();
+  this.updateProgress(0, 0);
 };
 
 LoadingBar.prototype.setStyling = function () {
-    'use strict';
+  'use strict';
 
-    this.element.style.height = this.barHeight + "px";
-    this.element.style.marginTop = "-" + (this.barHeight / 2) + "px";
-    this.element.style.backgroundColor = this.barColor;
-    this.element.style.position = "absolute";
-    this.element.style.top = "50%";
+  this.element.style.height = this.barHeight + 'px';
+  this.element.style.marginTop = '-' + (this.barHeight / 2) + 'px';
+  this.element.style.backgroundColor = this.barColor;
+  this.element.style.position = 'absolute';
+  this.element.style.top = '50%';
 
-    this.setTransitionTime(100);
+  this.setTransitionTime(100);
 };
 
 LoadingBar.prototype.updateProgress = function (percentage, time) {
-    'use strict';
+  'use strict';
 
-    if (parseInt(percentage) < 0) {
-        percentage = 0;
-    } else if (parseInt(percentage) > 100) {
-        percentage = 100;
-    }
+  if (parseInt(percentage) < 0) {
+    percentage = 0;
+  } else if (parseInt(percentage) > 100) {
+    percentage = 100;
+  }
 
-    if (time !== 0) {
-        this.setTransitionTime(time);
-    }
+  if (time !== 0) {
+    this.setTransitionTime(time);
+  }
 
-    this.element.style.width = parseInt(percentage) + "%";
+  this.element.style.width = parseInt(percentage) + '%';
 };
 
 LoadingBar.prototype.setTransitionTime = function (ms) {
-    "use strict";
-    this.element.style.WebkitTransition = "width " + ms + "ms";
-    this.element.style.MozTransition = "width " + ms + "ms";
-    this.element.style.OTransition = "width " + ms + "ms";
-    this.element.style.MsTransition = "width " + ms + "ms";
-    this.element.style.Transition = "width " + ms + "ms";
+  'use strict';
+  this.element.style.WebkitTransition = 'width ' + ms + 'ms';
+  this.element.style.MozTransition = 'width ' + ms + 'ms';
+  this.element.style.OTransition = 'width ' + ms + 'ms';
+  this.element.style.MsTransition = 'width ' + ms + 'ms';
+  this.element.style.Transition = 'width ' + ms + 'ms';
 };
 
 module.exports = function() {
