7605bd85f39d8af5a09ba18110912fc0ec40cc6d
Gaya Kessler <gaya.kessler@bluemango.nl>
2015-10-14 16:52:48 +0200

    use percentage parse module

--- src/Overlay/Percentage.js
+++ src/Overlay/Percentage.js
@@ -1,38 +1,32 @@
 'use strict';
-var Percentage = {};
+var PercentageParser = require('./PercentageParser');
 
-Percentage.prototype.create = function () {
-  'use strict';
-  this.element = document.createElement('div');
-  this.element.setAttribute('class', this.className);
-  this.element.setAttribute('id', this.idName);
-  this.applyStyling();
-  this.updateProgress(0, 0);
-};
+var Percentage = {
+  create: function() {
+    this.element = document.createElement('div');
+    this.element.setAttribute('class', this.className);
+    this.element.setAttribute('id', this.idName);
+    this.applyStyling();
+    this.updateProgress(0, 0);
+  },
 
-Percentage.prototype.applyStyling = function () {
-  'use strict';
-  this.element.style.height = '40px';
-  this.element.style.width = '100%';
-  this.element.style.position = 'absolute';
-  this.element.style.fontSize = '3em';
-  this.element.style.top = '50%';
-  this.element.style.left = '0';
-  this.element.style.marginTop = '-' + (59 + this.barHeight) + 'px';
-  this.element.style.textAlign = 'center';
-  this.element.style.color = this.barColor;
-};
+  applyStyling: function() {
+    this.element.style.height = '40px';
+    this.element.style.width = '100%';
+    this.element.style.position = 'absolute';
+    this.element.style.fontSize = '3em';
+    this.element.style.top = '50%';
+    this.element.style.left = '0';
+    this.element.style.marginTop = '-' + (59 + this.barHeight) + 'px';
+    this.element.style.textAlign = 'center';
+    this.element.style.color = this.barColor;
+  },
 
-Percentage.prototype.updateProgress = function (percentage, time) {
-  'use strict';
+  updateProgress: function(percentage, time) {
+    percentage = PercentageParser(percentage);
 
-  if (parseInt(percentage) < 0) {
-    percentage = 0;
-  } else if (parseInt(percentage) > 100) {
-    percentage = 100;
-  }
-
-  this.element.innerHTML = parseInt(percentage) + '%';
+    this.element.innerHTML = percentage + '%';
+  },
 };
 
 module.exports = function() {
