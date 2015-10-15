a3911486a6f49820204a07d78c1390c86421bf17
Gaya Kessler <gaya.kessler@bluemango.nl>
2015-10-15 13:14:19 +0200

    image to module

--- src/ImagePreloader/Image.js
+++ src/ImagePreloader/Image.js
@@ -1,26 +1,28 @@
+'use strict';
 var ImageLoaded = require('./ImageLoaded.js');
 
-function Image(src) {
-  'use strict';
-  this.src = src;
-  this.element = null;
+var Image = {
+  create: function() {
+    this.element = document.createElement('img');
+    this.element.setAttribute('src', this.src);
+  },
+
+  preload: function(cb) {
+    ImageLoaded(this.element, function(err, alreadyLoaded) {
+      cb(err, alreadyLoaded);
+    });
+  },
+};
+
+module.exports = function(src) {
+  var image = Object.create(Image);
+
+  image.src = src;
+  image.element = null;
 
   if (typeof src !== 'undefined') {
-    this.create();
+    image.create();
   }
-}
 
-Image.prototype.create = function () {
-  'use strict';
-  this.element = document.createElement('img');
-  this.element.setAttribute('src', this.src);
+  return image;
 };
-
-Image.prototype.preload = function(cb) {
-  'use strict';
-  ImageLoaded(this.element, function(err, alreadyLoaded) {
-    cb(err, alreadyLoaded);
-  });
-};
-
-module.exports = Image;
