f92f74c93a4cd03f2f9caee6ff790e4c6a6aa3e9
Gaya Kessler <gaya.kessler@bluemango.nl>
2015-10-15 13:50:48 +0200

    ImagePreloader to module

--- src/ImagePreloader/index.js
+++ src/ImagePreloader/index.js
@@ -1,149 +1,141 @@
+'use strict';
 var Image = require('./Image.js');
 
-function ImagePreloader(parent) {
-    "use strict";
-    this.parent = parent;
-    this.sources = [];
-    this.images = [];
-    this.loaded = 0;
-    this.deepSearch = true;
-}
-
-ImagePreloader.prototype.getImageSrcs = function (element) {
-    "use strict";
+var ImagePreloader = {
+  getImageSrcs: function(element) {
     this.sources = [];
 
-    if (typeof element !== "undefined") {
-        this.findImageInElement(element);
+    if (typeof element !== 'undefined') {
+      this.findImageInElement(element);
 
-        if (this.deepSearch === true) {
-            var elements = element.querySelectorAll("*");
-            for (var i = 0; i < elements.length; i++) {
-                if (elements[i].tagName !== "SCRIPT") {
-                    this.findImageInElement(elements[i]);
-                }
-            }
+      if (this.deepSearch === true) {
+        var elements = element.querySelectorAll('*');
+        for (var i = 0; i < elements.length; i++) {
+          if (elements[i].tagName !== 'SCRIPT') {
+            this.findImageInElement(elements[i]);
+          }
         }
+      }
     }
 
     return this.sources;
-};
+  },
 
-ImagePreloader.prototype.findAndPreload = function (element) {
-    "use strict";
-    if (typeof element === "undefined") {
-        return;
+  findAndPreload: function(element) {
+    if (typeof element === 'undefined') {
+      return;
     }
 
     this.sources = this.getImageSrcs(element);
 
     for (var i = 0; i < this.sources.length; i++) {
-        var image = new Image(this.sources[i]);
-        image.preload(this.imageLoaded.bind(this));
-        this.images.push(image);
+      var image = new Image(this.sources[i]);
+      image.preload(this.imageLoaded.bind(this));
+      this.images.push(image);
     }
-};
+  },
 
-ImagePreloader.prototype.imageLoaded = function () {
-    "use strict";
+  imageLoaded: function() {
     this.loaded++;
 
     this.updateProgress();
-};
+  },
 
-ImagePreloader.prototype.updateProgress = function () {
-    "use strict";
+  updateProgress: function() {
     this.parent.updateProgress(this.loaded, this.sources.length);
-};
+  },
 
-ImagePreloader.prototype.findImageInElement = function (element) {
-    "use strict";
+  findImageInElement: function(element) {
     var urlType = this.determineUrlAndType(element);
 
     //skip if gradient
     if (!this.hasGradient(urlType.url)) {
-        //remove unwanted chars
-        urlType.url = this.stripUrl(urlType.url);
+      //remove unwanted chars
+      urlType.url = this.stripUrl(urlType.url);
 
-        //split urls
-        var urls = urlType.url.split(", ");
+      //split urls
+      var urls = urlType.url.split(', ');
 
-        for (var i = 0; i < urls.length; i++) {
-            if (this.validUrl(urls[i]) && this.urlIsNew(urls[i])) {
-                var extra = "";
+      for (var i = 0; i < urls.length; i++) {
+        if (this.validUrl(urls[i]) && this.urlIsNew(urls[i])) {
+          var extra = '';
 
-                if (this.isIE() || this.isOpera()){
-                    //filthy always no cache for IE, sorry peeps!
-                    extra = "?rand=" + Math.random();
-                }
+          if (this.isIE() || this.isOpera()) {
+            //filthy always no cache for IE, sorry peeps!
+            extra = '?rand=' + Math.random();
+          }
 
-                //add image to found list
-                this.sources.push(urls[i] + extra);
-            }
+          //add image to found list
+          this.sources.push(urls[i] + extra);
         }
+      }
     }
-};
+  },
 
-ImagePreloader.prototype.determineUrlAndType = function (element) {
-    "use strict";
-    var url = "";
-    var type = "normal";
+  determineUrlAndType: function(element) {
+    var url = '';
+    var type = 'normal';
     var style = element.currentStyle || window.getComputedStyle(element, null);
 
-    if ((typeof style.backgroundImage !== "undefined" && style.backgroundImage !== "" && style.backgroundImage !== "none")
-      || (typeof element.style.backgroundImage !== "undefined" && element.style.backgroundImage !== "" && element.style.backgroundImage !== "none")
+    if ((typeof style.backgroundImage !== 'undefined' && style.backgroundImage !== '' && style.backgroundImage !== 'none')
+      || (typeof element.style.backgroundImage !== 'undefined' && element.style.backgroundImage !== '' && element.style.backgroundImage !== 'none')
     ) {
-        //if object has background image
-        url = (style.backgroundImage || element.style.backgroundImage);
-        type = "background";
-    } else if (typeof(element.getAttribute("src")) !== "undefined" && element.nodeName.toLowerCase() === "img") {
-        //if is img and has src
-        url = element.getAttribute("src");
+      //if object has background image
+      url = (style.backgroundImage || element.style.backgroundImage);
+      type = 'background';
+    } else if (typeof element.getAttribute('src') !== 'undefined' && element.nodeName.toLowerCase() === 'img') {
+      //if is img and has src
+      url = element.getAttribute('src');
     }
 
     return {
-        url: url,
-        type: type
+      url: url,
+      type: type,
     };
-};
+  },
 
-ImagePreloader.prototype.hasGradient = function (url) {
-    "use strict";
-    return (url && typeof url.indexOf !== "undefined" ? url.indexOf("gradient(") !== -1 : false );
-};
+  hasGradient: function(url) {
+    return (url && typeof url.indexOf !== 'undefined' ? url.indexOf('gradient(') !== -1 : false);
+  },
 
-ImagePreloader.prototype.stripUrl = function (url) {
-    "use strict";
-    url = url.replace(/url\(\"/g, "");
-    url = url.replace(/url\(/g, "");
-    url = url.replace(/\"\)/g, "");
-    url = url.replace(/\)/g, "");
+  stripUrl: function(url) {
+    url = url.replace(/url\(\'/g, '');
+    url = url.replace(/url\(/g, '');
+    url = url.replace(/\'\)/g, '');
+    url = url.replace(/\)/g, '');
 
     return url;
-};
+  },
 
-ImagePreloader.prototype.validUrl = function (url) {
-    "use strict";
+  validUrl: function(url) {
     if (url.length > 0 && !url.match(/^(data:)/i)) {
-        return true;
+      return true;
     } else {
-        return false;
+      return false;
     }
-};
+  },
 
-ImagePreloader.prototype.urlIsNew = function (url) {
-    "use strict";
+  urlIsNew: function(url) {
     return this.sources.indexOf(url) === -1;
-};
+  },
 
-ImagePreloader.prototype.isIE = function () {
-    "use strict";
+  isIE: function() {
     return navigator.userAgent.match(/msie/i);
-};
+  },
 
-ImagePreloader.prototype.isOpera = function () {
-    "use strict";
+  isOpera: function() {
     return navigator.userAgent.match(/Opera/i);
+  },
 };
 
-module.exports = ImagePreloader;
+module.exports = function(parent) {
+  var imagePreloader = Object.create(ImagePreloader);
+
+  imagePreloader.parent = parent;
+  imagePreloader.sources = [];
+  imagePreloader.images = [];
+  imagePreloader.loaded = 0;
+  imagePreloader.deepSearch = true;
+
+  return imagePreloader;
+};
