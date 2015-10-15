cf3cd9217b038629a08c6572e19918bc7cad2e5a
Gaya Kessler <gaya.kessler@bluemango.nl>
2015-10-15 11:59:41 +0200

    reformat bind polyfil

--- src/Polyfills/index.js
+++ src/Polyfills/index.js
@@ -1,25 +1,26 @@
 if (!Function.prototype.bind) {
-    Function.prototype.bind = function (oThis) {
-        "use strict";
-        if (typeof this !== "function") {
-            // closest thing possible to the ECMAScript 5
-            // internal IsCallable function
-            throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-        }
+  Function.prototype.bind = function (oThis) {
+    'use strict';
+    if (typeof this !== 'function') {
+      // closest thing possible to the ECMAScript 5
+      // internal IsCallable function
+      throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+    }
 
-        var aArgs = Array.prototype.slice.call(arguments, 1),
-            fToBind = this,
-            fNOP = function () {},
-            fBound = function () {
-                return fToBind.apply(this instanceof fNOP && oThis
-                    ? this
-                    : oThis,
-                    aArgs.concat(Array.prototype.slice.call(arguments)));
-            };
+    var aArgs = Array.prototype.slice.call(arguments, 1);
+    var _this = this;
+    var FNOP = function() {};
 
-        fNOP.prototype = this.prototype;
-        fBound.prototype = new fNOP();
+    var fBound = function() {
+        return _this.apply(this instanceof FNOP && oThis
+            ? this
+            : oThis,
+          aArgs.concat(Array.prototype.slice.call(arguments)));
+      };
 
-        return fBound;
-    };
+    FNOP.prototype = this.prototype;
+    fBound.prototype = new FNOP();
+
+    return fBound;
+  };
 }
